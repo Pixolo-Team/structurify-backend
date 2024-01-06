0ac6ff77c6ef86fbaed0255883c9b06ae5723429
Abhay Amin <abhay.pixolo@gmail.com>
2024-01-06 20:37:31 +0530

    Added Next JS files

--- technology/next/template.tsx
+++ technology/next/template.tsx
@@ -1,7 +1,38 @@
-// TYPES //
+"use client";
+import React from "react";
+
+// STYLES //
 
 // COMPONENTS //
 
-// NEXT //
+// IMAGES //
 
-const HomeScreen = () => {}
+// SERVICES //
+
+// TYPES //
+
+// UTILS //
+
+// PLUGINS //
+
+/** |PascalCase| Screen */
+const |PascalCase|: React.FC<unknown> = () => {
+	// Navigation and Route Params
+	
+	// Define States
+
+  // Define Refs
+
+  // Helper Functions
+
+  // UseEffect Functions and UseFocusEffect Functions
+	
+  // View starts here
+	return (
+		<div>
+			Hello |PascalCase|
+		</div>
+	);
+};
+
+export default |PascalCase|;
